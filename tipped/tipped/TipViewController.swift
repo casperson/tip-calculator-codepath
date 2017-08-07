e20e05fd98f3330360977906fe6140f31a9ce5a1
Braden Casperson <bradencasperson@Bradens-MacBook-Pro.local>
2017-08-06 21:45:12 -0700

    First commit with basic calculator set out👶

---
+++ tipped/tipped/TipViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TipViewController.swift
+//  tipped
+//
+//  Created by Braden Casperson on 8/6/17.
+//  Copyright © 2017 Braden Casperson. All rights reserved.
+//
+
+import UIKit
+
+class TipViewController: UIViewController {
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String.init(format: "$%.2f", tip)
+        totalLabel.text = String.init(format: "$%.2f", total)
+    }
+}
+
