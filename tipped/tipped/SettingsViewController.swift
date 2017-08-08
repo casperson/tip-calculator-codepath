dfb111087d11989017e65156def0cdac391902ed
Braden Casperson <bcasper@l-sff161gtf1-m.gap.com>
2017-08-08 11:23:45 -0700

    Added settings view and pan gesture recognizer 🤘

--- tipped/tipped/SettingsViewController.swift
+++ tipped/tipped/SettingsViewController.swift
@@ -8,28 +8,76 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UITableViewController {
+    
+    @IBOutlet weak var defaultTextField: UITextField!
+    @IBOutlet weak var minimumTextField: UITextField!
+    @IBOutlet weak var maximumTextField: UITextField!
+    
+    var defaultSettings = ["Default", "Minimum", "Maxiumum"]
+    var otherSettings = ["Dark Mode", "History"]
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        defaultTextField.text = String(defaults.integer(forKey: "default"))
+        maximumTextField.text = String(defaults.integer(forKey: "maximum"))
+        minimumTextField.text = String(defaults.integer(forKey: "minimum"))
+        
+        defaultTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingDidEnd)
+        minimumTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingDidEnd)
+        maximumTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingDidEnd)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func darkModeSwitch(_ sender: Any) {
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func textFieldDidChange() {
+        defaults.set(Int(defaultTextField.text!), forKey: "default")
+        defaults.set(Int(minimumTextField.text!), forKey: "minimum")
+        defaults.set(Int(maximumTextField.text!), forKey: "maximum")
+        defaults.synchronize()
     }
-    */
+    
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        return 2
+//    }
+    
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        if section == 0 {
+//            return defaultSettings.count
+//        } else {
+//            return otherSettings.count
+//        }
+//    }
+    
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        if section == 0 {
+//            return "Tip Percentage"
+//        } else {
+//            return "Other"
+//        }
+//    }
+    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        if indexPath.section == 0 {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell") as UITableViewCell!
+//            cell?.textLabel?.text = self.defaultSettings[indexPath.row]
+//            return cell!
+//        } else if indexPath.row == 0{
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "DarkModeCell") as UITableViewCell!
+//            cell?.textLabel?.text = self.otherSettings[indexPath.row]
+//            return cell!
+//        } else {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryCell") as UITableViewCell!
+//            cell?.textLabel?.text = "History"
+//            return cell!
+//        }
+//    }
 
 }
--- tipped/tipped/TipViewController.swift
+++ tipped/tipped/TipViewController.swift
@@ -11,35 +11,97 @@ import UIKit
 class TipViewController: UIViewController {
     
     @IBOutlet weak var tipLabel: UILabel!
-    @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var percentageLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipView: UIView!
+    
+    var newTranslation = CGPoint(x: 0.0, y: 0.0)
+    var previousTranslation = CGPoint(x: 0.0, y: 0.0)
+    var maximum: Int = 0
+    var minimum: Int = 0
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        billField.becomeFirstResponder()
+        
+        percentageLabel.text = String(defaults.integer(forKey: "default"))
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        maximum = defaults.integer(forKey: "maximum")
+        minimum = defaults.integer(forKey: "minimum")
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(TipViewController.keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
     }
-
+  
+    func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            
+            let screenSize: CGRect = UIScreen.main.bounds
+            let height = UIApplication.shared.statusBarFrame.height + self.navigationController!.navigationBar.frame.height
+            tipView.frame = CGRect(x: 0, y: height, width: screenSize.width, height: screenSize.height - height - keyboardHeight)
+        }
+    }
+   
+    func checkPercentage(current: Int, velocity: CGFloat) -> Bool {
+        if velocity > 0 && current == maximum {
+            return false
+        } else if velocity < 0 && current == minimum {
+            return false
+        }
+        
+        return true
+    }
+    
+    // MARK: IBActions
+    
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
-
-    @IBAction func calculateTip(_ sender: Any) {
+    
+    @IBAction func onSwipe(_ sender: UIPanGestureRecognizer) {
+        let piece = sender.view
         
-        let tipPercentages = [0.18, 0.2, 0.25]
+        if sender.state == .began || sender.state == .changed {
+            var currentPercent = Int(percentageLabel.text!)
         
-        
+            if checkPercentage(current: currentPercent!, velocity: sender.velocity(in: piece).x) {
+                if sender.velocity(in: piece).x > 0 {
+                    currentPercent = currentPercent! + 1
+                    percentageLabel.text = String(describing: currentPercent!)
+                } else if sender.velocity(in: piece).x < 0 {
+                    currentPercent = currentPercent! - 1
+                    percentageLabel.text = String(describing: currentPercent!)
+                }
+            }
+            calcTip()
+        }
+    }
+    
+    func calcTip() {
         let bill = Double(billField.text!) ?? 0
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let tipPercentage = Double(percentageLabel.text!)! / 100
+        let tip = bill * tipPercentage
         let total = bill + tip
         
         tipLabel.text = String.init(format: "$%.2f", tip)
         totalLabel.text = String.init(format: "$%.2f", total)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        calcTip()
     }
 }
 
